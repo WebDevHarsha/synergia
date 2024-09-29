7cf927e099140e437365f275d418388cfcd5ced4
sriharsha2942005 <sriharsha2942005@gmail.com>
2024-09-29 05:52:31 +0000

    home page link edit

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ function page() {
           </div>
 
           <br></br>
-          <Link href="/post-event" passHref>
+          <Link href="/events" passHref>
             <button className="relative inline-flex items-center justify-center p-0.5 mb-4 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
               <span className="relative px-4 py-2 md:px-5 md:py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 Post Your Event Now ᐳ
